9b703597d4fc01be9f4c510c6b4738286a71b384
Shankaranarayanansk <sankaranarayananit002@gmail.com>
2024-08-15 22:23:14 +0530

    pro url added

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -6,7 +6,7 @@ import img from '../assets/img.png';
 import { FcGoogle } from "react-icons/fc";
 
 // Hardcoded API URL - replace with your actual API URL
-const apiUrl = "http://localhost:9000";
+const apiUrl = "https://todo-oauth-mern-shankaranarayanansk.onrender.com/";
 
 const Todo = () => {
   const [title, setTitle] = useState('');
