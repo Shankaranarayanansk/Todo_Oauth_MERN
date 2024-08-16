b3c819c5021ae439f0f3ac36a7c0c855fedab511
Shankaranarayanan <127618183+Shankaranarayanansk@users.noreply.github.com>
2024-08-16 13:43:39 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,5 +2,5 @@ const jwt = require("jsonwebtoken");
 
 exports.googleCallback = (req, res) => {
   const token = jwt.sign({ id: req.user.id }, process.env.JWT_SECRET || "your_jwt_secret", { expiresIn: "1h" });
-  res.redirect(`http://localhost:9000?token=${token}`);
+  res.redirect(`https://todo-oauth-mern-shankaranarayanansk.onrender.com?token=${token}`);//changed line 
 };
