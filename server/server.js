3b01c3f77a4a894411b6598dbfe95506deb75b82
Shankaranarayanansk <sankaranarayananit002@gmail.com>
2024-08-21 22:34:57 +0530

    Pro url

--- server/server.js
+++ server/server.js
@@ -64,7 +64,7 @@ if(process.env.NODE_ENV==="production")
   })
 }
 
-const port = process.env.PORT || 9001;
+const port = process.env.PORT || 9000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
